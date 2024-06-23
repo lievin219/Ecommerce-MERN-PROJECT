d42d99bb68ec5a69120b6f342288c27da3710bc1
lievin219 <gakizalievin219@gmail.com>
2024-06-23 21:01:04 +0200

    mern

---
+++ src/Components/NewCollections/NewCollextions.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './NewCollection.css'
+import new_collections from '../Assets/Assets/new_collections'
+import item from '../Items/Items.jsx'
+
+const NewCollextions = () => {
+  return (
+    <div className='newcollection'>
+        <h1>New Collection </h1>
+        <hr />
+        <div className='collections'>
+            {new_collections.map((item,i)=>{
+             return <item/>
+            })}
+
+        </div>
+      
+    </div>
+  )
+}
+
+export default NewCollextions
