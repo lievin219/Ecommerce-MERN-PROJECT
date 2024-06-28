9074bace16a635e1fca6b86179a8bfbfb5a3ab34
lievin219 <gakizalievin219@gmail.com>
2024-06-28 22:26:59 +0200

    update all things

--- src/Components/Items/Items.jsx
+++ src/Components/Items/Items.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './Items.css'
+import { Link } from 'react-router-dom'
 
 const Items = (props) => {
   return (
     <div className='item'>
-              <img src={props.image} alt="" />
+              <Link to={`/product/${props.id}`}><img src={props.image} alt="" /></Link>
                 <p>{props.name}</p>
         <div className="item-prices">
             <div className="item-price-new">
---
+++ src/Components/productDisplay/ProductDisplay.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const ProductDisplay = () => {
+  return (
+    <div className='productdisplay'>
+        <div lassName='productdisplay-left'>
+
+        </div>
+          <div className='productdisplay-right'>
+
+          </div>
+      
+    </div>
+  )
+}
+
+export default ProductDisplay
