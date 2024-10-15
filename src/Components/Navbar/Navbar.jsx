ef2de540fe6650aabe41ea8bbef67bde2ae051a6
lievin219 <gakizalievin219@gmail.com>
2024-10-15 22:15:07 +0200

    usenavigate:package imported to help in redirecting on my login page

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -73,7 +73,7 @@ const Navbar = () => {
         Cookies.remove('authTokenii', { path: '/' });
         setAuthToken(null); 
         window.location.reload() // Update state immediately
-        window.location.replace('/login')
+        window.location.replace('./pages/login')
      
          
     };
